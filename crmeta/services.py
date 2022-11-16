5532f907b4d472a8ddcac25def73e3594514a393
Andreas Neumeier <andreas@neumeier.org>
2022-11-16 12:50:35 +0100

    start support for github API.

---
+++ crmeta/services.py
@@ -0,0 +1,13 @@
+from github import Github
+
+
+class GitHubWrapper(object):
+    """
+    Wrap `PyGithub` for use in Web Projects
+    """
+    def __init__(self, token: str) -> None:
+        self.token = token
+        self.github = Github(self.token)
+
+    def repos(self) -> List:
+        return self.github.get_user().get_repos()
