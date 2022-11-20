8ce81a10e3b329d05c62bbaad403cd9828a803bc
Andreas <andreas@neumeier.org>
2022-11-20 14:55:06 +0100

    ...

--- crmeta/tests.py
+++ crmeta/tests.py
@@ -4,3 +4,6 @@ from django.test import TestCase
 class MetaTests(TestCase):
     def test_meta(self):
         self.assertEqual(True, True)
+
+    def test_antimeta(self):
+        self.assertEqual(False, False)
